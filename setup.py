cfa0a87ed1c34d0e102279365ef4365d9b4a146d
Daniel Vaccaro-Senna <daniel.vaccaro-senna@accantosystems.com>
2019-08-21 15:24:33 +0100

    2.0.7.1 patch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lmctl',
-    version='2.0.7',
+    version='2.0.7.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
