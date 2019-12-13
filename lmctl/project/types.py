22bf2912bd8b03b6a242fa074a5aa0efcf36ce5f
Daniel Vaccaro-Senna <daniel.vaccaro-senna@accantosystems.com>
2019-12-13 14:36:35 +0000

    Support projects for Types. Ensure base configuration IDs are replaced

--- lmctl/project/types.py
+++ lmctl/project/types.py
@@ -1,5 +1,6 @@
 ASSEMBLY_PROJECT_TYPE = 'Assembly'
 RESOURCE_PROJECT_TYPE = 'Resource'
+TYPE_PROJECT_TYPE = 'Type'
 NS_PROJECT_TYPE = 'NS'
 VNF_PROJECT_TYPE = 'VNF'
 
@@ -24,3 +25,8 @@ def is_resource_type(project_type):
     if project_type is None:
         return False
     return project_type.lower() in [RESOURCE_PROJECT_TYPE.lower()]
+
+def is_type_project_type(project_type):
+    if project_type is None:
+        return False
+    return project_type.lower() in [TYPE_PROJECT_TYPE.lower()]
