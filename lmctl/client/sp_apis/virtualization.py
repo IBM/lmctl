3754acf380cb9399e16e2f8b61ac7313cf3b1efa
Steve Glover <steven.glover@gmail.com>
2021-10-10 10:25:54 +0100

    Add get VPC by cloud provider vpc id

--- lmctl/client/sp_apis/virtualization.py
+++ lmctl/client/sp_apis/virtualization.py
@@ -169,6 +169,13 @@ class VPCsAPI(SitePlannerCrudAPI):
         ).json()
         return [self._record_to_dict(r) for r in resp.get('results', [])]
 
+    def get_by_cloud_provider_vpc_id(self, id: str) -> List:
+        resp = self._make_direct_http_call(
+            verb='get',
+            override_url=self._pynb_endpoint.url + f'/?cloud_provider={id}',
+        ).json()
+        return [self._record_to_dict(r) for r in resp.get('results', [])]
+
     def build(self, id: str) -> str:
         response = self._make_direct_http_call(
             verb='post',
